7dd48adcbc1d97a8aabce90832142d850d688f66
Muhammad Ali <133556679+coder-muhammad@users.noreply.github.com>
2025-08-03 18:37:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const PORT = 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
-    res.send({path: path.join(__dirname, 'public', 'index.html')});
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log("Server is up and running..");
+  console.log(`🚀 Server is up and running on http://localhost:${PORT}`);
 });
