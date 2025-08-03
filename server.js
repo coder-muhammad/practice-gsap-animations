31812dfdc378561c9faa0ddcc575dfc7b546d85b
Muhammad Ali <133556679+coder-muhammad@users.noreply.github.com>
2025-08-03 18:38:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is up and running on http://localhost:${PORT}`);
+  console.log(`Server is up and running on http://localhost:${PORT}`);
 });
