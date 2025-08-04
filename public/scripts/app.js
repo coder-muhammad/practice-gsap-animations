524a2310c329641ff350452559507f78056b02b1
Muhammad Ali <133556679+coder-muhammad@users.noreply.github.com>
2025-08-04 13:09:00 +0500

    Practice Tween

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,25 @@
-gsap.to(".box", { 
+gsap.to(".green", {
+  rotation: 27,
   x: 200,
   duration: 2,
-})
+});
+
+gsap.to(".blue", { 
+  rotation: 360,
+  x: 100,
+  duration: 3,
+});
+
+gsap.from(".purple", { 
+  rotation: -360,
+  x: -100,
+  duration: 3,
+});
+
+gsap.fromTo(".red",
+  { x: -100 }, 
+  { 
+    rotation: 360, 
+    x: 100, 
+    duration: 3,
+  });
